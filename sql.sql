a5507fae3599ab0bb50c9d49305b626dd2882ac0
zyx <zyx1478948@qq.com>
2024-04-21 21:28:48 +0800

    下拉菜单

---
+++ sql.sql
@@ -0,0 +1,27 @@
+-- tp5.yunzhi_web definition
+
+CREATE TABLE `yunzhi_web` (
+  `name` varchar(100) DEFAULT NULL
+) ENGINE=MyISAM AUTO_INCREMENT=30 DEFAULT CHARSET=utf8mb4;
+
+
+-- tp5.yunzhi_notice definition
+
+CREATE TABLE `yunzhi_notice` (
+  `Column1` varchar(100) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+
+
+-- tp5.yunzhi_download definition
+
+CREATE TABLE `yunzhi_download` (
+  `Column1` varchar(100) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+
+
+-- tp5.yunzhi_experiment definition
+
+
+CREATE TABLE `yunzhi_experiment` (
+  `Column1` varchar(100) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
